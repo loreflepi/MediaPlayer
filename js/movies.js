3e4685991b2a904d205833333f1ce58c7f3268af
Lorena Pinzón <lorena5pinzon@gmail.com>
2020-09-09 20:46:38 -0500

    Adding movie details

--- js/movies.js
+++ js/movies.js
@@ -1,25 +1,35 @@
 const buttonShow = document.getElementById("buttonShow");
 const apiKey = "b89fc45c2067cbd33560270639722eae";
 
-buttonShow.onclick = () => {
-    getMovies();
-}
+
+getMovies();
+
 
 async function getMovies(){
     const url = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${apiKey}`;
     const response = await fetch(url);
     const data = await response.json();
     let ids = [];
-    for(let i = 0; i < 5; i++){
+    for(let i = 0; i < 20; i++){
         ids.push(getMovie(data.results[i].id));
     }
     let result = await Promise.all(ids);
-    result.forEach(re => console.log(re));
+    const movieList = document.getElementById('container__movies');
+    movieList.innerHTML = '';
+    result.forEach(re => {
+        let domVariable = document.createElement("div");
+        domVariable.className = "container__movies--details";
+        domVariable.innerHTML = `   <img src="https://image.tmdb.org/t/p/w342${re.poster_path}">
+                                    <b>${re.title}</b>
+                                    <p>${re.release_date}</p>`;
+        movieList.appendChild(domVariable);
+    });
 }
 
 async function getMovie(id){
     const urlFirsts = `https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}`;
     const responseMovies = await fetch(urlFirsts);
     const dataMovies = await responseMovies.json();
+    const movieImage = `https://image.tmdb.org/t/p/w342${dataMovies.poster_path}`;
     return dataMovies;
 }
